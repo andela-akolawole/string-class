f898d85dccd93c87cd12cddf69234498df0e0e94
andela-akolawole <aladekolawole15@yahoo.com>
2016-10-27 16:23:07 +0100

    finishes isQuestion-feature

--- tests/spec/string-class-spec.js
+++ tests/spec/string-class-spec.js
@@ -1,79 +1,78 @@
 'use strict'
 
-describe('String class test', () => {
+describe('String class test', function () {
     
-    describe('String.hasVowels test', () => {
-
-        it('should check if it is defined', () => {
-            let string = "";
+    describe('String.hasVowels test', function () {
+        var string = "";
+        it('should check if it is defined', function () {
             expect(string.hasVowels()).toBeDefined();
         });
 
-        it('should return true if the string contains vowels', () => {
-            let string = "cwme";
+        it('should return true if the string contains vowels', function () {
+            var string = "cwme";
             expect(string.hasVowels()).toBeTruthy();
         });
 
-        it('should return false if string does not contain vowels', () => {
-            let string = "c";
+        it('should return false if string does not contain vowels', function () {
+            var string = "c";
             console.log(string.hasVowels());
             expect(string.hasVowels()).toBe(false);
         });
     });
 
-    describe('String.toUpper test', () => {
+    describe('String.toUpper test', function () {
        
-       it('should check if it is defined', () => {
-            let string = "";
+       it('should check if it is defined', function () {
+            var string = "";
             expect(string.toUpper()).toBeDefined();
         });
 
-       it('should return an uppercase string', () => {
-            let string = "lowercase";
+       it('should return an uppercase string', function () {
+            var string = "lowercase";
             expect(string.toUpper()).toBe('LOWERCASE');
        });
     });
 
-    describe('Sting.toLower test', () => {
+    describe('Sting.toLower test', function () {
 
-        it('should check if it is defined', () => {
-            let string = "";
+        it('should check if it is defined', function () {
+            var string = "";
             expect(string.toLower()).toBeDefined();
         });
         
-        it('should return a lowercase string', () => {
-            let string = "LOWERCASE";
+        it('should return a lowercase string', function () {
+            var string = "LOWERCASE";
             expect(string.toLower()).toBe('lowercase');
         });
     });
 
-    describe('String.ucFirst test', () => {
+    describe('String.ucFirst test', function () {
 
-        it('should check if it is defined', () => {
-            let string = "";
+        it('should check if it is defined', function () {
+            var string = "";
             expect(string.ucFirst()).toBeDefined();
         });
         
-        it('should return a string with the first letter in uppercase', () => {
-            let string = "jude";
+        it('should return a string with the first varter in uppercase', function () {
+            var string = "jude";
             expect(string.ucFirst()).toBe('Jude');
         });
     });
 
-    describe('String.isQuestion', () => {
+    describe('String.isQuestion', function () {
 
-        it('should check if it is defined', () => {
-            let string = "what is lower?";
+        it('should check if it is defined',function () {
+            var string = "what is lower?";
             expect(string.isQuestion()).toBeDefined();
         });
 
-        it('should return true if string is a question', () => {
-            let string = "What did you say?";
+        it('should return true if string is a question', function () {
+            var string = "What did you say?";
             expect(string.isQuestion()).toBeTruthy();
         });
 
-        it('should return false if string is not a question', () => {
-            let string = "shoot them all";
+        it('should return false if string is not a question', function () {
+            var string = "shoot them all";
             expect(string.isQuestion()).toBeFalsy();
         });
     });
